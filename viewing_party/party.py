594a0b4cb85a88eb129fc2c3fb1ce36f8155123b
dehuihu <dehuihu@gmail.com>
2025-03-25 13:28:15 -0700

    Finished Wave_02 and passed all tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -32,8 +32,36 @@ def watch_movie(user_data, title):
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+    # for empty list
+    if not user_data["watched"]:
+        return 0.0
+    
+    total_rating = 0.0
+    # iterate through list to add up all ratings
+    for movie in user_data["watched"]:
+        total_rating += movie["rating"]
 
+    return total_rating / len(user_data["watched"])
 
+def get_most_watched_genre(user_data):
+    #check if watched list is empty
+    if not user_data["watched"]:
+        return None
+    # create a dictionary to store genre and counts. 
+    genre_count = {}
+    highest_count = 0
+    most_common_genre = ""
+    # assumption: for multiple genres with same highest_count, just return 1st appearance
+    for movie in user_data["watched"]:
+        if movie["genre"] in genre_count:
+            genre_count[movie["genre"]] += 1
+            if genre_count[movie["genre"]] > highest_count:
+                highest_count = genre_count[movie["genre"]]
+                most_common_genre = movie["genre"]
+        else: 
+            genre_count[movie["genre"]] = 1
+    return most_common_genre 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
