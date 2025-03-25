6cd69ff77c8a8c388a9a106c65ac9234e3b1746d
dehuihu <dehuihu@gmail.com>
2025-03-25 15:18:39 -0700

    Implemented wave_04 and debugging test "no_available_Friends_rec"

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,3 +1,4 @@
+#Assumption: all strings are in upper case
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
@@ -95,7 +96,13 @@ def get_friends_unique_watched(user_data):
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
-
+def get_available_recs(user_data):
+    movies = get_friends_unique_watched(user_data)
+    for movie in movies:
+        if movie["host"] not in user_data["subscriptions"]:
+            movies.remove(movie)
+        
+    return movies
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
